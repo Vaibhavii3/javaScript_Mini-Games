21789364677ef2637aff54852b4fbc70ce3879ad
Vaibhavi <vaibhavichaudhari304@gmail.com>
2024-06-24 20:22:39 +0530

    added timer

--- Games/script.js
+++ Games/script.js
@@ -1,27 +1,71 @@
+// let hitCounts = 0;
+
+//     let isGameStarted = false;
+//     function startGame(){
+//         document.getElementById('gameStatus').innerText = "Game Started !!";
+//         hitCounts = 0;
+        
+//         let alien = document.getElementById('alien');
+//         let time = setInterval(() => {
+//             let i = Math.floor(Math.random()*500)+1;
+//             let j = Math.floor(Math.random()*420)+1;
+//             alien.style.left = i + "px"
+//             alien.style.top = j + "px"
+//         },1000);
+
+//         setTimeout(() =>{
+//             clearTimeout(time);
+//             isGameStarted = false;
+//             document.getElementById('gameStatus').innerText = "Game Over !!";
+//         }, 30000)
+//     }
+
+//     function hitAlien(){
+//         if(isGameStarted){
+
+//             hitCounts++;
+//             document.getElementById('totalHits').innerText = hitCounts;
+//         }
+//     }
+
 let hitCounts = 0;
-    let isGameStarted = false;
-    function startGame(){
-        document.getElementById('gameStatus').innerText = "Game Started !!";
-        hitCounts = 0;
-        isGameStarted = true;
-        let alien = document.getElementById('alien');
-        let time = setInterval(() => {
-            let i = Math.floor(Math.random()*500)+1;
-            let j = Math.floor(Math.random()*420)+1;
-            alien.style.left = i + "px"
-            alien.style.top = j + "px"
-        },1000);
-        setTimeout(() =>{
-            clearTimeout(time);
+let isGameStarted = false;
+
+function startGame(){
+    if(isGameStarted) return;
+
+    isGameStarted = true;
+    hitCounts = 0;
+    document.getElementById('totalHits').innerText = hitCounts;
+    document.getElementById('gameStatus').innerText = "Game Started !!";
+    document.getElementById('timeLeft').innerText = 30;
+
+    let alien = document.getElementById('alien');
+    let timeLeft = 30;
+
+    let moveAlienInterval = setInterval(() => {
+        let i = Math.floor(Math.random() * 500) + 1;
+        let j = Math.floor(Math.random() * 420) + 1;
+        alien.style.left = i + "px";
+        alien.style.top = j + "px";
+    }, 1000);
+
+    let countdownInterval = setInterval(() => {
+        timeLeft--;
+        document.getElementById('timeLeft').innerText = timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(moveAlienInterval);
+            clearInterval(countdownInterval);
             isGameStarted = false;
             document.getElementById('gameStatus').innerText = "Game Over !!";
-        }, 30000)
-    }
-
-    function hitAlien(){
-        if(isGameStarted){
-
-            hitCounts++;
-            document.getElementById('totalHits').innerText = hitCounts;
         }
+    }, 1000);
+
+}
+
+function hitAlien(){
+    if(isGameStarted){
+        hitCounts++;
+        document.getElementById('totalHits').innerText = hitCounts;
     }
+}
